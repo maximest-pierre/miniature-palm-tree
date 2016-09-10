a079f3a3aaad86bb0fdf49e008668b39911ff253
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-09-10 10:23:46 -0400

    Fix problem with urls route

--- django/miniature_palm_tree/miniature_palm_tree/urls.py
+++ django/miniature_palm_tree/miniature_palm_tree/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^inventory', include('inventory.urls', namespace=inventory)),
+    url(r'^inventory', include('inventory.urls', namespace="inventory")),
 ]
