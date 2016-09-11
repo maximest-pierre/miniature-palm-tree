38fb0e6ee10a5e9f4be340f6c631cd555c93effb
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-09-11 09:26:44 -0400

    Add base model for the inventory

--- django/miniature_palm_tree/inventory/models.py
+++ django/miniature_palm_tree/inventory/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class ItemGroup (models.Model):
+    pass
+
+class Item (models.Model):
+
+    name = models.CharField()
+    creation_date = models.DateField()
+    arrival_date = models.DateField()
