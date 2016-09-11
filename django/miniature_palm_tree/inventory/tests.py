77917ba6df2aa1bc00872d40bb8ac44c0734f4b8
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-09-11 09:33:32 -0400

    Add basic test to the item model

--- django/miniature_palm_tree/inventory/tests.py
+++ django/miniature_palm_tree/inventory/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from inventory.models import Item, ItemGroup
+from datetime import date
 
-# Create your tests here.
+class ItemTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        item = Item.objects.create(
+            name="123", creation_date=date.today, arrival_date=date.today
+        )
+
+    def testName(self):
+        item = Item.objects.get(name="123")
+        self.assertEqual(item.name, "123")
+
+    def testCreationDate(self):
+        item = Item.objects.get(name="123")
+        self.assertEqual(item.creation_date, date.today)
+
+    def testArrivalDate(self):
+        item = Item.objects.get(name="123")
+        self.assertEqual(item.arrival_date, date.today)
