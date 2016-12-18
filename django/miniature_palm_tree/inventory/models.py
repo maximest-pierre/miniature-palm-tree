4b3a5bcb430a57165eebb6eb56da44d9172a1608
maximest-pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-12-17 23:48:58 -0400

    fix pep8 and fix problem with model

--- django/miniature_palm_tree/inventory/models.py
+++ django/miniature_palm_tree/inventory/models.py
@@ -2,12 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
 class ItemGroup (models.Model):
     pass
 
+
 class Item (models.Model):
 
-    name = models.CharField()
+    name = models.CharField(max_length=150)
     creation_date = models.DateField()
     arrival_date = models.DateField()
